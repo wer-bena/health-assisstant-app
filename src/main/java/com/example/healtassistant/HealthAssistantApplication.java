c84e8fe375805f797ec5cddf6793644789b942ee
Bernadeta G <grudzien.bernadeta@example.com>
2021-07-07 21:31:47 +0200

    Product model

--- src/main/java/com/example/healtassistant/HealthAssistantApplication.java
+++ src/main/java/com/example/healtassistant/HealthAssistantApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class HealthAssistantAppApplication {
+public class HealthAssistantApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(HealthAssistantAppApplication.class, args);
+		SpringApplication.run(HealthAssistantApplication.class, args);
 	}
 
 }
---
+++ src/main/java/com/example/healtassistant/model/Product.java
@@ -0,0 +1,32 @@
+package com.example.healtassistant.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Data
+@Builder(toBuilder = true)
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "products")
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    private int calories;
+
+    private float proteins;
+
+    private float carbohydrates;
+
+    private float fats;
+
+}
