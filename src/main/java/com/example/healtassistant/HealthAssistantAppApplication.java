d78c956281ef965f74f18653e7127860098e03a0
Bernadeta G <grudzien.bernadeta@example.com>
2021-07-07 18:44:44 +0200

    Initial commit

---
+++ src/main/java/com/example/healtassistant/HealthAssistantAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.healtassistant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HealthAssistantAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HealthAssistantAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/healtassistant/HealthAssistantAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.healtassistant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HealthAssistantAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
